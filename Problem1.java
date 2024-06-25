adc726c0eb5fb3a2bacfa7847660fdaa823d83fe
Nithin Kumar Pamidimukkala <nik@Nithins-MacBook-Pro.local>
2024-06-24 17:07:40 -0700

    Completed Competitive-Coindg-1

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,30 @@
+public class Problem1 {
+    //Problem1 =  https://www.geeksforgeeks.org/find-the-missing-number-in-a-sorted-array/
+    private static int findMissingBinary(int[] nums) { //O(logN)
+        int length = nums.length;
+        int low = 0;
+        int high = length;
 
+        if(nums[0] != 1) return 1; //missing first value
+        if(nums[length-1] == length) return length + 1; //missing last value
+
+        while(low <= high) {
+            int mid = low + (high - low)/2;
+            if(nums[mid] != mid + 1 && nums[mid-1] == mid) { //if mid-value is not correct but mid-1 is correct
+                return mid+1;
+            }
+            else if(nums[mid] != mid + 1 ) {
+                high = mid-1;
+            }
+            else  {
+                low = mid+1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,5,6,7,8};
+        System.out.println("Binary missing value is " + findMissingBinary(nums));
+    }
+}
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,109 @@
+public class Problem2 {
+    // Problem 2 is https://www.geeksforgeeks.org/min-heap-in-java/#
+    private static int[] heap;
+    private static int capacity;
+    private static int size;
 
+    public Problem2(int capacity) {
+        this.capacity = capacity;
+        this.size = 0;
+        heap = new int[capacity+1];
+        heap[0] = Integer.MIN_VALUE;
+    }
+
+    private static int parent(int pos) {
+        return pos/2;
+    }
+
+    private static int leftChild(int pos) {
+        return 2*pos;
+    }
+
+    private static int rightChild(int pos) {
+        return 2*pos + 1;
+    }
+
+    private int peak() {
+        return heap[1];
+    }
+
+    private static boolean isLeaf(int pos) {
+        return (size/2 < pos && pos <= size);
+    }
+
+    private void print() {
+        for(int i = 1; i <= size/2; i++) {
+            System.out.println("Parent is: " + heap[i]
+            + " Left child is: " + heap[leftChild(i)] +
+                    " Right child is: " + heap[rightChild(i)]);
+        }
+    }
+
+    private static void swap(int prev, int current) { //helper function
+        int temp = heap[prev];
+        heap[prev] = heap[current];
+        heap[current] = temp;
+    }
+
+    private void insert(int val) {
+        if(size >= capacity) return;
+
+        heap[++size] = val;
+        int current = size;
+        while(heap[parent(current)] > heap[current]) {
+            swap(parent(current), current);
+            current = parent(current);
+        }
+    }
+
+    private void heapify(int current) {
+        if(!isLeaf(current)) {
+            int smallest;
+            if(heap[leftChild(current)] < heap[current]) {
+                smallest = leftChild(current);
+            } else {
+                smallest = current;
+            }
+            if(heap[rightChild(current)] < heap[smallest]) {
+                smallest = rightChild(current);
+            }
+            if(smallest != current) {
+                swap(smallest, current);
+                heapify(smallest);
+            }
+        }
+    }
+
+    private int remove() {
+        int popped = heap[1];
+        heap[1] = heap[size--];
+        heapify(1);
+        return popped;
+    }
+
+
+    public static void main(String[] args) {
+        Problem2 problem2 = new Problem2(11);
+        problem2.insert(0);
+        problem2.insert(6);
+        problem2.insert(5);
+        problem2.insert(1);
+        problem2.insert(9);
+        System.out.println("Peak is: " + problem2.peak());
+        System.out.println("First print");
+        problem2.print();
+        problem2.insert(3);
+        problem2.insert(4);
+        problem2.insert(10);
+        System.out.println("Insert 3,4,10 and print");
+        problem2.print();
+        problem2.insert(8);
+        problem2.insert(2);
+        System.out.println("Insert 2,8 and print");
+        problem2.print();
+        System.out.println("Removing and print");
+        System.out.println("Removed is: " + problem2.remove());
+        System.out.println("Peak is: " + problem2.peak());
+        problem2.print();
+    }
+}
